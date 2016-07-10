ae6c5c25720bc0263fa1832236ebfe4c8c452c1a
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-07-10 13:24:33 +0900

    stop

--- src/main/java/com/ranceworks/rmwatermark/App.java
+++ src/main/java/com/ranceworks/rmwatermark/App.java
@@ -1,12 +1,8 @@
 package com.ranceworks.rmwatermark;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -37,9 +33,9 @@ public class App {
 			PdfDictionary dic = reader.getPageN(p);
 			System.out.println(p);
 			cln(dic);
+			// visit(dic);
 		}
 
-		// visit(dic);
 		Document document = new Document();
 		PdfCopy copy = new PdfCopy(document, new FileOutputStream("hoge.pdf"));
 		document.open();
@@ -66,13 +62,13 @@ public class App {
 		boolean found = false;
 		PdfName foundName = null;
 		for (PdfName name : dic.getKeys()) {
-			if (dic.get(name).isString()) {
-				PdfString str = (PdfString) dic.get(name);
+			if (dic.getDirectObject(name).isString()) {
+				PdfString str = (PdfString) dic.getDirectObject(name);
 				found = true;
 				foundName = name;
 				System.out.println("");
 			} else {
-				visit(dic.get(name));
+				visit(dic.getDirectObject(name));
 			}
 		}
 		if (found) {
@@ -82,8 +78,11 @@ public class App {
 	}
 
 	private void visit(PdfObject pdfObj) {
+
 		if (pdfObj.isString()) {
 			PdfString a = (PdfString) pdfObj;
+			System.out.println("");
+		} else if (pdfObj.isIndirect()) {
 			System.out.println("");
 		} else if (pdfObj.isDictionary()) {
 			PdfDictionary dic = (PdfDictionary) pdfObj;
@@ -99,40 +98,9 @@ public class App {
 
 	private void visit(final PdfArray array) {
 		for (int i = 0; i < array.size(); i++) {
-			if (array.getDirectObject(i).getBytes() != null) {
-				if (new String(array.getDirectObject(i).getBytes()).contains("")) {
-					System.out.println("");
-				}
-			}
-
 			visit(array.getDirectObject(i));
 		}
 
 	}
 
-	private List<PdfDictionary> flatDict(PdfDictionary dic) {
-
-		List<PdfObject> objects = dic.getKeys().stream().map(a -> dic.get(a)).collect(Collectors.toList());
-
-		List<PdfDictionary> dictionaries = objects.stream().filter(o -> o.isDictionary()).map(o -> (PdfDictionary) o)
-				.collect(Collectors.toList());
-
-		Stream.of(new File(".").listFiles())
-				.flatMap(file -> file.listFiles() == null ? Stream.of(file) : Stream.of(file.listFiles()))
-				.collect(Collectors.toList());
-
-		List<PdfDictionary> dictionaries2 = Stream.of((PdfDictionary[]) dictionaries.toArray())
-				.flatMap(m -> Stream.of((PdfDictionary[]) flatDict(m).toArray())).collect(Collectors.toList());
-		/*
-		 * 
-		 * 
-		 * List<PdfObject> dics = objects.stream().filter(o ->
-		 * o.isDictionary()).map(a -> flatDict((PdfDictionary) a)) .flatMap(c ->
-		 * Stream.of((PdfObject[]) c.toArray())).collect(Collectors.toList());
-		 */
-
-		List<PdfObject> notDicts = objects.stream().filter(o -> !o.isDictionary()).collect(Collectors.toList());
-
-		return null;
-	}
 }
