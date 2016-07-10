6e7a16ca86917dc9b43df8c8bbeae2a1fa05d98e
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-07-10 12:34:26 +0900

    before huristic

--- src/main/java/com/ranceworks/rmwatermark/App.java
+++ src/main/java/com/ranceworks/rmwatermark/App.java
@@ -1,18 +1,22 @@
 package com.ranceworks.rmwatermark;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfArray;
+import com.itextpdf.text.pdf.PdfCopy;
 import com.itextpdf.text.pdf.PdfDictionary;
 import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfObject;
 import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfString;
 
 /**
  * Hello world!
@@ -24,39 +28,69 @@ public class App {
 		new App().operate();
 	}
 
-	private void operate() throws IOException {
+	private void operate() throws IOException, DocumentException {
 		final PdfReader reader = new PdfReader(getClass().getResourceAsStream("test.pdf"));
-
+		HashMap<String, String> a = reader.getInfo();
+		a.clear();
 		PdfDictionary dic = reader.getPageN(1);
 
-		visit(dic);
+		PdfArray array = (PdfArray) dic.get(PdfName.CONTENTS);
+		array.remove(array.size() - 1);
+		// visit(dic);
+		Document document = new Document();
+		PdfCopy copy = new PdfCopy(document, new FileOutputStream("hoge.pdf"));
+		document.open();
+		copy.addDocument(reader);
+		document.close();
+		reader.close();
 	}
 
-	private void visit(PdfDictionary dic) {
-
-		dic.getKeys().stream().filter(k -> PdfName.ANNOTS.equals(k)).forEach((PdfName k) -> {
-			visit(dic.get(k));
-		});
+	private void visit(final PdfDictionary dic) {
+		boolean found = false;
+		PdfName foundName = null;
+		for (PdfName name : dic.getKeys()) {
+			if (dic.get(name).isString()) {
+				PdfString str = (PdfString) dic.get(name);
+				found = true;
+				foundName = name;
+				System.out.println("");
+			} else {
+				visit(dic.get(name));
+			}
+		}
+		if (found) {
+			dic.put(foundName, new PdfString(""));
+		}
 
 	}
 
 	private void visit(PdfObject pdfObj) {
-
-		if (pdfObj.isDictionary()) {
+		if (pdfObj.isString()) {
+			PdfString a = (PdfString) pdfObj;
+			System.out.println("");
+		} else if (pdfObj.isDictionary()) {
 			PdfDictionary dic = (PdfDictionary) pdfObj;
 			visit(dic);
 		} else if (pdfObj.isArray()) {
 			PdfArray array = (PdfArray) pdfObj;
 			visit(array);
+		} else if (pdfObj.isStream()) {
+			PdfDictionary stm = (PdfDictionary) pdfObj;
+			visit(stm);
 		}
 	}
 
 	private void visit(final PdfArray array) {
-		if (!array.isEmpty()) {
-			IntStream.range(0, array.size()).forEach(i -> {
-				visit(array.getDirectObject(i));
-			});
+		for (int i = 0; i < array.size(); i++) {
+			if (array.getDirectObject(i).getBytes() != null) {
+				if (new String(array.getDirectObject(i).getBytes()).contains("")) {
+					System.out.println("");
+				}
+			}
+
+			visit(array.getDirectObject(i));
 		}
+
 	}
 
 	private List<PdfDictionary> flatDict(PdfDictionary dic) {
